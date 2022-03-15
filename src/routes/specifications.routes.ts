4ef1d7a499f3ba878c6435dc846dc8d93e7d2d92
Lugui14 <zanelallopes9977@gmail.com>
2022-03-15 11:22:59 -0300

    finalized: authenticate and error treatment

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,13 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+
+import { ensureAuthenticate } from "../middlewares/ensureAuthenticate";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationRoutes.use(ensureAuthenticate);
 specificationRoutes.post("/", createSpecificationController.handle);
 
 export { specificationRoutes };
